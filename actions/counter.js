141ec39efb523c48d452fafab0a131557005cae8
steven03.chen <steven03.chen@vipshop.com>
2015-12-11 19:35:36 +0800

    ''

--- actions/counter.js
+++ actions/counter.js
@@ -1,34 +1,56 @@
-export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
-export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
 
 export function increment() {
-  return {
-    type: INCREMENT_COUNTER
-  }
+	
+	
+	return {
+		type : 'increment',
+		state : {
+			n : state.n++
+		}
+	}
+}
+
+increment.reducer = 'count';
+
+function reducer(state, action) {
+	return Object.assign({}, state, action.state);
+}
+
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+	export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+	export function increment() {
+	return {
+		type : INCREMENT_COUNTER
+	}
 }
 
 export function decrement() {
-  return {
-    type: DECREMENT_COUNTER
-  }
+	return {
+		type : DECREMENT_COUNTER
+	}
 }
 
 export function incrementIfOdd() {
-  return (dispatch, getState) => {
-    const { counter } = getState()
+	return (dispatch, getState) =  > {
+		const {
+			counter
+		}
+			 = getState()
 
-    if (counter.n % 2 === 0) {
-      return
-    }
+			if (counter.n % 2 === 0) {
+				return
+			}
 
-    dispatch(increment())
-  }
+			dispatch(increment())
+	}
 }
 
 export function incrementAsync(delay = 1000) {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(increment())
-    }, delay)
-  }
+	return dispatch =  > {
+		setTimeout(() =  > {
+				dispatch(increment())
+			}, delay)
+	}
 }
